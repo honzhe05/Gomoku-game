963fcdf130a25c9c28b933ee219b5d07e76fd501
honzhe05 <honzhe05@gmail.com>
2026-06-01 13:27:04 +0800

    fix js

--- board.js
+++ board.js
@@ -1,41 +1,11 @@
 const API = "https://gomoku-game-xs81.onrender.com";
 
 const size = 15;
-let board = [];
 const cells = [];
 let player = 1;
 let game_id;
 
 const boardDiv = document.getElementById("Board");
-
-function renderBoard(board) {
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      const cell = cells[i][j];
-      cell.innerHTML = "";
-
-      if (
-        (i === 3 && j === 3) ||
-        (i === 7 && j === 7) ||
-        (i === 3 && j === 11) ||
-        (i === 11 && j === 3) ||
-        (i === 11 && j === 11)
-      ) {
-        const dot = document.createElement("div");
-        dot.className = "star-point";
-        cell.appendChild(dot);
-      }
-
-      if (board[i][j] !== 0) {
-        const stone = document.createElement("div");
-        stone.className =
-          "stone " + (board[i][j] === 1 ? "black" : "white");
-
-        cell.appendChild(stone);
-      }
-    }
-  }
-}
 
 function setStone(i, j, player) {
   const cell = cells[i][j];
@@ -53,14 +23,23 @@ function createBoard() {
   boardDiv.innerHTML = "";
 
   for (let i = 0; i < size; i++) {
-    board[i] = [];
     cells[i] = [];
 
     for (let j = 0; j < size; j++) {
-      board[i][j] = 0;
-
       const cell = document.createElement("div");
       cell.className = "cell";
+      
+      if (
+        (i === 3 && j === 3) ||
+        (i === 7 && j === 7) ||
+        (i === 3 && j === 11) ||
+        (i === 11 && j === 3) ||
+        (i === 11 && j === 11)
+      ) {
+        const dot = document.createElement("div");
+        dot.className = "star-point";
+        cell.appendChild(dot);
+      }
 
       if (i === 0) cell.classList.add("top");
       if (i === size - 1) cell.classList.add("bottom");
@@ -139,13 +118,16 @@ async function placeStone(row, col) {
 
 async function resetGame() {
   if (!game_id) return;
-  
+
   await fetch(`${API}/reset`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ game_id })
   });
 
+  cells.length = 0;
+
+  createBoard();
+
   player = 1;
-  await reDraw();
 }
